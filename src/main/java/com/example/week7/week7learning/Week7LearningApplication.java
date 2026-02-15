011ea0d689cbb2d5c93961e2b6abb776ee210a29
Hemant <hem789ant1997@outlook.com>
2026-02-15 23:26:46 +0530

    first commit

---
+++ src/main/java/com/example/week7/week7learning/Week7LearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.week7.week7learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week7LearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week7LearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/week7/week7learning/Week7LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.week7.week7learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week7LearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
