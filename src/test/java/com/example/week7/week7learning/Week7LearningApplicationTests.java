baa24b72215130113b638239d9a3fe6b11764d18
Hemant <hem789ant1997@outlook.com>
2026-02-26 13:38:00 +0530

    added test during build8

--- src/test/java/com/example/week7/week7learning/Week7LearningApplicationTests.java
+++ src/test/java/com/example/week7/week7learning/Week7LearningApplicationTests.java
@@ -4,13 +4,34 @@ import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.data.Offset;
 import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import javax.sql.DataSource;
+import java.util.TimeZone;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @Slf4j
 @SpringBootTest
+@Import(TestContainerConfiguration.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class Week7LearningApplicationTests {
+
+    @Autowired
+    DataSource ds;
+
+    @Test
+    void checkUrl() throws Exception {
+        System.out.println(ds.getConnection().getMetaData().getURL());
+    }
+
+    @BeforeAll
+    static void setTimezone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
     @Test
         //    @Disabled
@@ -18,7 +39,6 @@ class Week7LearningApplicationTests {
     }
 
     @BeforeEach
-    @Test
     void setUp(){
         log.info("Starting the method, setting up the config");
     }
@@ -31,7 +51,6 @@ class Week7LearningApplicationTests {
 
 
     @AfterEach
-    @Test
     void tearDown(){
         log.info("Tearing down the method");
     }
